ed1bf1ec74c6a9a156f04dcc1feb2e92d085ac94
Toby Williams <tobyplw@me.com>
2024-02-12 18:17:03 -0500

    Split Vocoder into separate class, updated ReadMe

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Audio-Obfuscation-Capstone
+
+Dependencies List:
+Note: Use Pip to install
+- numpy
+- pyaudio
+- scipy.io
+
+To use:
+1. Test_data holds ~8 instances of various people saying "This is an Audio Test"
+2. Run 'python3 create_test.py', this will combine all Test Data into a single Audio File
+3. Run 'python3 Vocoder_File_to_File.py), this will use the vocoding algorithm to obfuscare the test file and generate a new obfuscated file
+4. If you would like to hear your voice obfuscated in real time, run 'python3 Vocoder_Input_to_Output', which will take your input microphone and output to your system output. Make sure you are using headphones or it will echo. 
+
+
--- create_test.py
+++ create_test.py
@@ -1,24 +1,27 @@
-import wave
 import glob
 import random
 import numpy as np
-
 from scipy.io.wavfile import write, read
 
 infiles = glob.glob('./Test_data/*.wav')
-random.shuffle(infiles)
-print("FILES FOR TESTING")
-print(infiles)
+print("\nFILES FOR TESTING")
+i = 1
+for file in infiles:
+    print("[" + str(i) + "] File: " + file)
+    i+=1
 
+print("\nShuffling")
+random.shuffle(infiles)
 outfile = 'Test_Audio_Input.wav'
 
 all = np.array([], dtype = np.float32)
 
-rate = 0
+rate = 48000
 for wav_path in infiles:
     rate, in_data = read(wav_path)
     in_data = in_data.astype(np.float32, order = 'C') / 32768.0
     all = np.append(all, in_data)
 
 write(outfile, rate, all.astype(np.float32))
+print("\nFile <" + outfile + "> was created")
 
